0cf33cf61faf4dca5649857c05bd2bb1cb82a7d2
Nikhil2xd <nikhilmansotragg@gmail.com>
2024-06-28 23:21:49 +0530

    fixed legend issue - made level 1

--- temp.js
+++ temp.js
@@ -7,97 +7,115 @@
 
 const player = "p";
 const ball = "b";
-const goalposta = "ga";
-const goalpostb = "gb";
+const goalpost = "g";
 const hurdle = "w";
+const grass = "l";
+const bounds = "e";
 
 //legend
 setLegend(
   [ player, bitmap`
-......CCC.......
-....CCCCCC......
-....CCCC2CC.....
-....C2020CC.....
-.....22222CC....
-......222.......
-....777777......
-....777777......
-....277772......
-....277772......
-....155551......
-.....5555.......
-.....2..2.......
-.....2..2.......
-.....L..L.......
-.....LL.LL......`],
+444444CCC4444444
+4444CCCCCC444444
+4444CCCC2CC44444
+4444C2020CC44444
+4444422222CC4444
+4444442224444444
+4444777777444444
+4444777777444444
+4444277772444444
+4444277772444444
+4444155551444444
+4444455554444444
+4444424424444444
+4444424424444444
+44444L44L4444444
+44444LL4LL444444`],
   [ ball, bitmap`
-................
-................
-................
-................
-................
-....000.........
-..00L2L00.......
-.0LLL22220......
-.0LLL2LL20......
-022222LL220.....
-022222LL2L0.....
-0LLL22222L0.....
-.0LL222220......
-.0222LLL20......
-..002LL00.......
-....000.........`],
-  [ goalposta, bitmap`
-.......00000....
-.......0LLL00...
-.......0000L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........000...`],
-  [ goalpostb, bitmap`
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-..........0L0...
-.......0000L0...
-.......0LLL00...
-.......00000....`],
+4444444444444444
+4444444444444444
+4444444444444444
+4444440004444444
+444400L2L0044444
+4440LLL222204444
+4440LLL2LL204444
+44022222LL220444
+44022222LL2L0444
+440LLL22222L0444
+4440LL2222204444
+4440222LLL204444
+4444002LL0044444
+4444440004444444
+4444444444444444
+4444444444444444`],
+  [ goalpost, bitmap`
+4000004444444444
+40LLL00444444444
+40000L0444444444
+44440L0444444444
+44440L0444444444
+44440L0444444444
+44440L0444444444
+44440L0444444444
+44440L0444444444
+44440L0444444444
+44440L0444444444
+44440L0444444444
+44440L0444444444
+40000L0444444444
+40LLL00444444444
+4000004444444444`],
   [ hurdle, bitmap`
-......101.......
-.....11011......
-.....10001......
-....110L011.....
-.....10L01......
-....110L011.....
-.....10L01......
-....1102011.....
-.....10L01......
-....110L011.....
-.....10L01......
-....110L011.....
-.....10L01......
-....1100011.....
-.....11011......
-......101.......`]
+4444440L04444444
+4444410L01444444
+4444410L01444444
+4444110L01144444
+4444410L01444444
+4444110L01144444
+4444410L01444444
+4444110L01144444
+4444410L01444444
+4444110L01144444
+4444410L01444444
+4444110L01144444
+4444410L01444444
+4444110L01144444
+4444410L01444444
+4444440L04444444`],
+  [ grass, bitmap`
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444
+4444444444444444`],
+  [ bounds, bitmap`
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD
+DDDDDDDDDDDDDDDD`]
 );
 
 
@@ -105,13 +123,27 @@ setLegend(
 let level = 0; 
 const levels = [
   map`
-....
-p...
-....`,
+eeeeeeeeeeeeee
+elllwllwlwlwle
+elllwllwlwlwle
+ellllllwlllwle
+epblllllllllge
+elllwllllwlwle
+elllwllwlwlwle
+elllwllwlwllle
+eeeeeeeeeeeeee`,
   map`
-p..
-.b.
-..g`,
+....w...w...w.w.
+....w.w.....w.w.
+....w.w.w.w.w.w.
+........w.w.w.w.
+....w.w.w.....w.
+p.b.w.w.w......g
+....w.w.w.w.w.w.
+....w.....w.w.w.
+....w.w...w.w.w.
+....w.w.w.w.w.w.
+....w.w.w.w.w.w.`,
   map`
 p.wg
 .bw.
@@ -126,39 +158,31 @@ p...
 ...
 .p.
 ...`,
-  map`
-p.w.
-.bwg
-....
-..bg`
 ];
 
 
 const currentLevel = levels[level];
 setMap(currentLevel);
 
-setSolids([ player, ball, hurdle ]); 
+setSolids([ player, ball, hurdle, bounds]); 
 
 // movement of player
 onInput("s", () => {
   getFirst(player).y += 1; 
-  playTune(move);
-});
+  });
 
 onInput("d", () => {
   getFirst(player).x += 1;
-  playTune(move);
-});
+  });
 
 onInput("w", () => {
     getFirst(player).y -= 1;
-    playTune(move);
-});
+    });
 
 onInput("a", () => {
     getFirst(player).x -= 1;
-    playTune(move);
-});
+    });
+
 onInput("s", () => {
   getFirst(player).y += 1
 })
@@ -172,14 +196,18 @@ if (currentLevel !== undefined) {
  }
 });
 
+// for ball
+setPushables({
+  [player]: [ ball ],
+});
+
 
 afterInput(() => {
   
-  const targetNumber = tilesWith(goalpost1,goalpost2).length;
+  const targetNumber = tilesWith(goalpost).length;
   
-  const numberCovered = tilesWith(goalpost1, goalpost2, ball).length;
-
- 
+  const numberCovered = tilesWith(goalpost, ball).length;
+  
   if (numberCovered === targetNumber) {
    
     level = level + 1;
